3ac48ff24b7eb0ec89e24704c017990707787a95
Ben Bellick <benbellick@protonmail.com>
2023-02-15 17:40:43 -0500

    basic parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,55 @@
 module Main (main) where
 
-import Lib
+--import Lib
 
-{--
-main :: IO ()
-main = someFunc
---}
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+    _ <- char '"'
+    x <- many (noneOf "\"")
+    _ <- char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest  <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _val -> "Found value"
 
 main :: IO ()
 main = do
-    putStrLn "What is your first num?"
-    x <- getLine >>= (return . read)
-    y <- getLine >>= (return . read)
-    putStrLn $ "sum of nums is: " ++ show (x + y)
+    (expr:_) <- getArgs
+    putStrLn $ readExpr expr
