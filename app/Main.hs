c246485181044a875be37718bc8cc6141eb363a7
Ben Bellick <benbellick@protonmail.com>
2023-02-26 22:07:41 -0500

    Add REPL

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,41 @@ import Parser
 import Evaluator
 import Error
 import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad.Except
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
+evalStr :: String -> IO String
+evalStr expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalStr expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ predicate prompt action = do
+  val <- prompt
+  if predicate val
+  then return ()
+  else action val >> until_ predicate prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
---main = getArgs >>= print . eval . readExpr  . head
---main = getArgs >>= print . readExpr  . head
 main = do
   args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program requires 0 args for interactive or 1 arg for eval and print"
