37411626cf8edd199e6458a346597e62cec93b52
Ben Bellick <benbellick@protonmail.com>
2023-03-14 19:39:02 -0400

    Also moves show imple for LispError to Types

--- src/Error.hs
+++ src/Error.hs
@@ -3,17 +3,6 @@ module Error(trapError, extractValue) where
 import Types
 import Control.Monad.Except
 
-showError :: LispError -> String
-showError (UnboundVar msg varname) = msg ++ ": " ++ varname
-showError (BadSpecialForm msg form) = msg ++ ": " ++ show form
-showError (NotFunction msg func) = msg ++ ": " ++ show func
-showError (NumArgs expected found) =  "Expected " ++ show expected ++ " args; found values: " ++ show found
-showError (TypeMismatch expected found) =  "Invalid type: expected " ++ expected ++ ", found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ show parseErr
-showError (Default msg) = "Default error: " ++ msg
-
-instance Show LispError where show = showError
-
 trapError :: (MonadError e m, Show e) => m String -> m String
 trapError action = catchError action (return . show)
 
--- src/Types.hs
+++ src/Types.hs
@@ -69,5 +69,16 @@ data LispError = NumArgs Integer [LispVal]
                | UnboundVar String String
                | Default String
 
+showError :: LispError -> String
+showError (UnboundVar msg varname) = msg ++ ": " ++ varname
+showError (BadSpecialForm msg form) = msg ++ ": " ++ show form
+showError (NotFunction msg func) = msg ++ ": " ++ show func
+showError (NumArgs expected found) =  "Expected " ++ show expected ++ " args; found values: " ++ show found
+showError (TypeMismatch expected found) =  "Invalid type: expected " ++ expected ++ ", found " ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (Default msg) = "Default error: " ++ msg
+
+instance Show LispError where show = showError
+
 type ThrowsError = Either LispError
 type IOThrowsError = ExceptT LispError IO
